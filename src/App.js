2b43957f8d1bccdc05500b851686b242f443b5cd
georg <george25gehamnew@gmail.com>
2024-09-20 23:14:16 +0300

    Counter Using States in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [number, setNumber] = useState(0);
+  function plus() {
+    setNumber(number => number + 1);
+  }
+  function minus() {
+    setNumber(number => number - 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className='number'>{number}
+        </div>
+        <div className='plus' onClick={plus}>+</div>
+        <div className='minus' onClick={minus}>-</div>
+      </main>
     </div>
   );
 }
